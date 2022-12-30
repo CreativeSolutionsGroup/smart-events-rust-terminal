51638033b6345415b0c62ea9890994cd4c56e4df
Sebastian Neiswanger <sebastianneiswanger@gmail.com>
2022-12-30 13:41:42 -0500

    Database functions work

--- terminal_app/src/main.rs
+++ terminal_app/src/main.rs
@@ -1,10 +1,20 @@
 mod models;
+mod services;
+use futures::executor::block_on;
 use models::heartbeat::*;
 use models::checkin::*;
+use services::cache::*;
 
 fn main() {
     // Just a small example of how to use the models
     let beat: Heartbeat = Heartbeat { mac_address: "00:00:00:00:00:00".to_string() };
     let check_in: Checkin = Checkin { mac_address: beat.mac_address, student_id: "12345".to_string() };
     println!("mac_address: {}, student_id: {}", check_in.mac_address, check_in.student_id);
+
+    // Example of cache
+    // Use https://sqliteviewer.app/#/ to observe the sql table
+    let db_url = "sqlite://cache.db";
+    block_on(initialize_database(db_url));
+    block_on(insert_check_in(db_url, &check_in));
+    //block_on(delete_check_in(db_url, &check_in.student_id));
 }
--- terminal_app/src/services/cache.rs
+++ terminal_app/src/services/cache.rs
@@ -1,16 +1,61 @@
-use sqlx::Connection;
+use sqlx::{Sqlite, migrate::MigrateDatabase, SqlitePool};
+use crate::models::checkin::*;
 
-// Create a new database
-pub fn initialize_database() -> SqliteConnection {
-    SqliteConnection::conect("sqlite::memory:").await?;
+// Create a new database or connect to an existing one
+pub async fn initialize_database(url: &str) {
+    let db: SqlitePool;
+    // Tries to connect and if it can't connect then it will create a new database
+    match SqlitePool::connect(url).await {
+        Ok(pool) => { 
+            db = pool;
+        },
+        Err(_) => {
+            Sqlite::create_database(url).await.unwrap();
+            db = SqlitePool::connect(url).await.unwrap();
+        }
+    }
+    let create_table_str = 
+        "CREATE TABLE IF NOT EXISTS check_ins (
+            mac_address TEXT NOT NULL,
+            student_id  TEXT NOT NULL UNIQUE
+        );";
+    let _result = sqlx::query(create_table_str).execute(&db).await;
+    db.close().await;
 }
 
 // Deletes a given checkin with id from the cache.
-pub fn delete_check_in (db: SqliteConnection, id: String) {
-    db.execute(sqlx::query("DELETE FROM ")).await?;
+pub async fn delete_check_in (url: &str, id: &str) {
+    let db: SqlitePool;
+    match SqlitePool::connect(url).await {
+        Ok(pool) => { 
+            db = pool;
+        },
+        Err(_) => {
+            initialize_database(url).await;
+            db = SqlitePool::connect(url).await.unwrap();
+        }
+    }
+    let delete_checkin_str = 
+        format!("DELETE FROM check_ins WHERE student_id LIKE \"{}\";", id);
+    let _result = sqlx::query(&delete_checkin_str).execute(&db).await;
+    db.close().await;
 }
 
 // Inserts a checkin into the cache at the back
-pub fn insert_check_in (db: SqliteConnection, check_in: Checkin) {
-    db
+pub async fn insert_check_in (url: &str, check_in: &Checkin) {
+    let db: SqlitePool;
+    match SqlitePool::connect(url).await {
+        Ok(pool) => { 
+            db = pool;
+        },
+        Err(_) => {
+            initialize_database(url).await;
+            db = SqlitePool::connect(url).await.unwrap();
+        }
+    }
+    let insert_checkin_str = 
+        format!("INSERT INTO check_ins (mac_address, student_id) VALUES(\"{}\", \"{}\");", 
+                &check_in.mac_address, &check_in.student_id);
+    let _result = sqlx::query(&insert_checkin_str).execute(&db).await;
+    db.close().await;
 }
