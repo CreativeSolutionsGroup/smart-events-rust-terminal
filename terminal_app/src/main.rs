67959a42cfdf2add078b89d829ed658f457b7980
SebastianNeiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2023-01-03 17:15:46 -0500

    WIP on heartbeat

--- terminal_app/src/main.rs
+++ terminal_app/src/main.rs
@@ -1,5 +1,8 @@
 mod models;
 mod services;
+use std::env;
+use async_recursion::async_recursion;
+use std::{thread, time::Duration};
 use mac_address::MacAddress;
 use mac_address::get_mac_address;
 use futures::executor::block_on;
@@ -18,7 +21,43 @@ fn get_mac() -> MacAddress {
     }
 }
 
+#[async_recursion(?Send)]
+async fn build_heartbeat(mut backoff: u64) {
+    thread::sleep(Duration::from_secs(backoff));
+    
+    let default_url: &str = "tcp://localhost:3001";
+    let context = zmq::Context::new();
+    let p = context.socket(zmq::PUB).unwrap();
+    
+    let connection_url: String;
+    match env::var("heartbeat_url") {
+        Ok(url) => connection_url = url,
+        Err(_) => connection_url = default_url.to_string()
+    }
+
+    match p.connect(&connection_url) {
+        Ok(_) => (),
+        Err(_) => {
+            backoff *= 2;
+            if backoff > 128 { backoff = 128 } else if backoff == 0 { backoff = 1 }
+            println!("ZMQ Error. Attempting to reconnect in {} seconds", backoff);
+            build_heartbeat(backoff).await;
+        }
+    }
+
+    let client: Heartbeat = Heartbeat { mac_address: get_mac().to_string() };
+    
+    loop {
+        println!("sending");
+        p.send(&client.mac_address, 0).unwrap();
+        thread::sleep(Duration::from_secs(10));
+        backoff = 0;
+    }
+}
+
 fn main() {
+    build_heartbeat(0);
+
     // Just a small example of how to use the models
     let beat: Heartbeat = Heartbeat { mac_address: get_mac().to_string() };
     let check_in: Checkin = Checkin { mac_address: beat.mac_address, student_id: "12345".to_string() };
@@ -29,5 +68,5 @@ fn main() {
     let db_url = "sqlite://cache.db";
     block_on(initialize_database(db_url));
     block_on(insert_check_in(db_url, &check_in));
-    //block_on(delete_check_in(db_url, &check_in.student_id));
+    block_on(delete_check_in(db_url, &check_in.student_id));
 }
