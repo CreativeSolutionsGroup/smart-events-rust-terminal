f602422589e23862918f796b0c2f6f90a7fa7160
SebastianNeiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2023-01-04 14:28:05 -0500

    heartbeat changes

--- terminal_app/src/main.rs
+++ terminal_app/src/main.rs
@@ -1,14 +1,12 @@
 mod models;
 mod services;
-use std::env;
-use async_recursion::async_recursion;
-use std::{thread, time::Duration};
-use mac_address::MacAddress;
-use mac_address::get_mac_address;
-use futures::executor::block_on;
-use models::heartbeat::*;
-use models::checkin::*;
 use services::cache::*;
+use zmq::{Context, Socket};
+use futures::executor::block_on;
+use async_recursion::async_recursion;
+use std::{env, thread, time::Duration};
+use models::{heartbeat::*, checkin::*};
+use mac_address::{MacAddress, get_mac_address};
 
 fn get_mac() -> MacAddress {
     // gets the mac_address and returns it
@@ -25,18 +23,21 @@ fn get_mac() -> MacAddress {
 async fn build_heartbeat(mut backoff: u64) {
     thread::sleep(Duration::from_secs(backoff));
     
-    let default_url: &str = "tcp://localhost:3001";
-    let context = zmq::Context::new();
-    let p = context.socket(zmq::PUB).unwrap();
+    let default_url: &str = "tcp://localhost:9951";
+    let context: Context = zmq::Context::new();
+    let proxy: Socket = context.socket(zmq::PUB).unwrap();
     
+    println!("Trying to get env");
     let connection_url: String;
     match env::var("heartbeat_url") {
         Ok(url) => connection_url = url,
         Err(_) => connection_url = default_url.to_string()
     }
 
-    match p.connect(&connection_url) {
-        Ok(_) => (),
+    println!("{}", connection_url);
+
+    match proxy.connect(&connection_url) {
+        Ok(_) => println!("connected"),
         Err(_) => {
             backoff *= 2;
             if backoff > 128 { backoff = 128 } else if backoff == 0 { backoff = 1 }
@@ -45,11 +46,20 @@ async fn build_heartbeat(mut backoff: u64) {
         }
     }
 
+    println!("{}", proxy.get_socks_proxy().unwrap().unwrap());
+    
     let client: Heartbeat = Heartbeat { mac_address: get_mac().to_string() };
     
     loop {
-        println!("sending");
-        p.send(&client.mac_address, 0).unwrap();
+        match proxy.send(client.mac_address.as_bytes(), 0) {
+            Ok(_) => println!("sent heartbeat"),
+            Err(_) => {
+                backoff *= 2;
+                if backoff > 128 { backoff = 128 } else if backoff == 0 { backoff = 1 }
+                println!("ZMQ Error. Attempting to reconnect in {} seconds", backoff);
+                build_heartbeat(backoff).await;
+            }
+        }
         thread::sleep(Duration::from_secs(10));
         backoff = 0;
     }
