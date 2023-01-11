c43b2c16b953c21c6f38f84af9a0bfcf4d3a93be
SebastianNeiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2023-01-11 15:14:58 -0500

    added mutex for ZMQ locking

--- terminal_app/src/main.rs
+++ terminal_app/src/main.rs
@@ -3,7 +3,7 @@ mod services;
 use services::cache::*;
 use zmq::{Context, Socket};
 use futures::executor::block_on;
-use std::{env, thread, time::Duration};
+use std::{env, thread, time::Duration, sync::{Arc, Mutex}};
 use models::{heartbeat::*, checkin::*};
 use mac_address::{MacAddress, get_mac_address};
 
@@ -18,40 +18,43 @@ fn get_mac() -> MacAddress {
     }
 }
 
-fn build_heartbeat(mut backoff: u64) {
+fn build_heartbeat(mut backoff: u64, lock: Arc<Mutex<i8>>) {
     thread::sleep(Duration::from_secs(backoff));
-    
-    let default_url: &str = "tcp://localhost:9951";
-    let context: Context = zmq::Context::new();
-    let proxy: Socket = context.socket(zmq::PUB).unwrap();
 
-    let connection_url: String;
-    match env::var("heartbeat_url") {
-        Ok(url) => connection_url = url,
-        Err(_) => connection_url = default_url.to_string()
-    }
-
-    match proxy.connect(&connection_url) {
-        Ok(_) => println!("connected"),
-        Err(_) => {
-            backoff *= 2;
-            if backoff > 128 { backoff = 128 } else if backoff == 0 { backoff = 1 }
-            println!("ZMQ Error. Attempting to reconnect in {} seconds", backoff);
-            thread::spawn(move || {build_heartbeat(backoff)});
-            return;
-        }
-    }
-    
-    let client: Heartbeat = Heartbeat { mac_address: get_mac().to_string() };
-    
     loop {
+        let mutex_lock = lock.lock().unwrap();
+        let default_url: &str = "tcp://localhost:9951";
+        let context: Context = zmq::Context::new();
+        let proxy: Socket = context.socket(zmq::PUB).unwrap();
+
+        let connection_url: String;
+        match env::var("heartbeat_url") {
+            Ok(url) => connection_url = url,
+            Err(_) => connection_url = default_url.to_string()
+        }
+
+        match proxy.connect(&connection_url) {
+            Ok(_) => println!("connected"),
+            Err(_) => {
+                backoff *= 2;
+                if backoff > 128 { backoff = 128 } else if backoff == 0 { backoff = 1 }
+                println!("ZMQ Error. Attempting to reconnect in {} seconds", backoff);
+                std::mem::drop(mutex_lock);
+                thread::spawn(move || {build_heartbeat(backoff, lock)});
+                return;
+            }
+        }
+        
+        let client: Heartbeat = Heartbeat { mac_address: get_mac().to_string() };
+    
         match proxy.send(client.mac_address.as_bytes(), 0) {
             Ok(_) => println!("sent heartbeat"),
             Err(_) => {
                 backoff *= 2;
                 if backoff > 128 { backoff = 128 } else if backoff == 0 { backoff = 1 }
                 println!("ZMQ Error. Attempting to reconnect in {} seconds", backoff);
-                thread::spawn(move || {build_heartbeat(backoff)});
+                std::mem::drop(mutex_lock);
+                thread::spawn(move || {build_heartbeat(backoff, lock)});
                 return;
             }
         }
@@ -61,7 +64,13 @@ fn build_heartbeat(mut backoff: u64) {
 }
 
 fn main() {
-    let heartbeat_handle = thread::spawn(|| {build_heartbeat(0)});
+    // Mutex required for Windows as heartbeat and sqlite try to use the same memory location at the same time
+    let mutex_lock = Arc::new(Mutex::new(0));
+    let tread_arc_0 = mutex_lock.clone();
+    let tread_arc_1 = mutex_lock.clone();
+    let tread_arc_2 = mutex_lock.clone();
+
+    let heartbeat_handle = thread::spawn(move || {build_heartbeat(0, mutex_lock.clone())});
     // Just a small example of how to use the models
     let beat: Heartbeat = Heartbeat { mac_address: get_mac().to_string() };
     let check_in: Checkin = Checkin { mac_address: beat.mac_address, student_id: "12345".to_string() };
@@ -70,8 +79,8 @@ fn main() {
     // Example of cache
     // Use https://sqliteviewer.app/#/ to observe the sql table
     let db_url = "sqlite://cache.db";
-    block_on(initialize_database(db_url));
-    block_on(insert_check_in(db_url, &check_in));
-    block_on(delete_check_in(db_url, &check_in.student_id));
+    block_on(initialize_database(db_url, tread_arc_0));
+    block_on(insert_check_in(db_url, &check_in, tread_arc_1));
+    block_on(delete_check_in(db_url, &check_in.student_id, tread_arc_2));
     let _heartbeat_res = heartbeat_handle.join();
 }
