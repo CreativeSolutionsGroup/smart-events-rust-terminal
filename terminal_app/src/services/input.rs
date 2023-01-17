f473acdc4f60eee73be197185a801ea583218fad
Sebastian Neiswanger <sebastianneiswanger@gmail.com>
2023-01-17 15:52:16 -0500

    changed the entire cache'

--- terminal_app/src/services/input.rs
+++ terminal_app/src/services/input.rs
@@ -1,9 +1,8 @@
-use std::{sync::{Arc,Mutex}, io};
-use futures::executor::block_on;
+use std::io;
 use threadpool::ThreadPool;
 use crate::{models::checkin::*, services::{get_mac::*, cache::insert_check_in}};
 
-pub fn wait_for_input(lock: Arc<Mutex<i8>>) {
+pub fn wait_for_input() {
     let pool = ThreadPool::new(100);
     loop {
         println!("Input ID:");
@@ -11,12 +10,11 @@ pub fn wait_for_input(lock: Arc<Mutex<i8>>) {
      
         io::stdin().read_line(&mut id).expect("failed to readline");
 
-        let input_lock = lock.clone();
-        pool.execute(move || {save_input(id, input_lock.clone())});
+        pool.execute(move || {save_input(id)});
     }
 }
 
-fn save_input(mut id: String, lock: Arc<Mutex<i8>>) {
+fn save_input(mut id: String) {
     let id_length = 5;
 
     id = id.trim().to_string();
@@ -27,7 +25,7 @@ fn save_input(mut id: String, lock: Arc<Mutex<i8>>) {
                 // correct id parameters
                 let new_checkin: Checkin = Checkin { mac_address: get_mac().to_string(), student_id: id.trim().to_string() };
                 println!("mac_address: {}, student_id: {}", new_checkin.mac_address, new_checkin.student_id);
-                block_on(insert_check_in(&new_checkin, lock.clone()));
+                insert_check_in(&new_checkin);
             },
             Err(e) => println!("Did not have a correct student id. Recieved: {}", e),
         }
@@ -38,7 +36,7 @@ fn save_input(mut id: String, lock: Arc<Mutex<i8>>) {
                 // correct id parameters
                 let new_checkin: Checkin = Checkin { mac_address: get_mac().to_string(), student_id: mod_id.trim().to_string() };
                 println!("mac_address: {}, student_id: {}", new_checkin.mac_address, new_checkin.student_id);
-                block_on(insert_check_in(&new_checkin, lock.clone()));
+                insert_check_in(&new_checkin);
             },
             Err(e) => println!("Did not have a correct student id. Recieved: {}", e),
         }
