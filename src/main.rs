12a68507373c5db9a4e03c01f99d533a04a28225
Sebastian Neiswanger <sebastianneiswanger@gmail.com>
2023-01-30 16:14:11 -0500

    sends checkins over ZMQ and DELETES sent checkins

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 mod models;
 mod services;
-use services::{input::*, heartbeat::*, cache::initialize_database};
+use services::{input::*, heartbeat::*, cache::{initialize_database, cache_observer}};
 use std::thread;
 
 fn main() {
     initialize_database();
 
-    // This creates a new thread that contains the heartbeat
+    // Startup necessary threads
     let heartbeat_handle = thread::spawn(|| {build_heartbeat()});
-
-    // Start up the waiting for input
     let input_handle = thread::spawn(|| {wait_for_input()});
+    let chache_sender_handle = thread::spawn(|| {cache_observer()});
 
     // Threads must be joined back in or when main exits, it will force close any extra threads
     heartbeat_handle.join().unwrap();
     input_handle.join().unwrap();
+    chache_sender_handle.join().unwrap();
 }
